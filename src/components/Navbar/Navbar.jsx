a3c589fdc6c69815126ef0edc9290b91bccf3434
muhtasim50 <muhtasimfuad625@gmail.com>
2022-05-21 13:55:29 +0600

    3rd

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ const navbar = () => {
     <div className="n-wrapper" id="Navbar">
       {/* left */}
       <div className="n-left">
-        <div className="n-name">TH Unlimited</div>
+        <div className="n-name"><h3>Study Home</h3></div>
       </div>
       {/* right */}
       <div className="n-right">
